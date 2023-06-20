5dd03186a1a2703b3c8bf7db90177d24bf7aea93
daljmj <126189963+daljmj@users.noreply.github.com>
2023-06-20 16:54:05 +0900

    백엔드 개발 12 @RequestBody 사용하기

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -4,26 +4,23 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.example.demo.dto.TestRequestBodyDTO;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("test")
 public class TestController {
 	
-		@GetMapping
-		public String testController() {
-			return "Hello World!(안녕하세요!)";
+	@GetMapping
+	public String TestController(){
+		return "Hello World!";
 		}
-		@GetMapping("/{id}")
-		public String testControllerWithPathVariables(@PathVariable(required =false) int id){
-			return "Hello World! ID " + id;
+	@GetMapping("/testRequestBody")
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO 
+			testRequestBodyDTO){
+		return "Hello World! ID"+testRequestBodyDTO.getId()+" Message : "+ 
+			testRequestBodyDTO.getMessage();
 		}
-		
-		@GetMapping("/Param")
-		public String testControllerRequestParam(@RequestParam(required =false) int id){
-			return "Hello World! ID param " + id;
-		}
-		
-
 }
